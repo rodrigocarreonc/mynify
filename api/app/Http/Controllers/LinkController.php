fb9deaebe4af9333f3f07f493f4f41724c5f740a
rodrigocarreonc <rodrigocarreon21@gmail.com>
2025-01-03 23:07:58 -0600

    Backend and endpoint finished

---
+++ api/app/Http/Controllers/LinkController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Link;
+use Illuminate\Support\Str;
+
+class LinkController extends Controller
+{
+    public function short(Request $request){
+        $validated = $request->validate([
+            'url' => 'required|url'
+        ]);
+
+        $link = Link::create([
+            'url' => $validated['url'],
+            'hash' => Str::random(6),
+            'clicks' => 0
+        ]);
+
+        return response()->json($link);
+    }
+
+    public function redirect($hash){
+        $link = Link::where('hash', $hash)->firstOrFail();
+        $link->increment('clicks');
+        return redirect($link->url);
+    }
+}
